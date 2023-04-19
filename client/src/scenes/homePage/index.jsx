583c28c2512d659c72db16d1494471a2a0551207
Tom Gollick <thomasgollick@gmail.com>
2023-04-19 21:04:39 +0100

    Starting on frontend using MUI

---
+++ client/src/scenes/homePage/index.jsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+import React from "react";
+import Navbar from "../navbar";
+
+const HomePage = () => {
+  return (
+    <Box>
+      <Navbar />
+      https://www.youtube.com/watch?v=K8YELRmUb5o&t=5186s
+    </Box>
+  );
+};
+
+export default HomePage;
